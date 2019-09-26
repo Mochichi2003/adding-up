336a90adcf39c77113260ed5a4475f613ef5a3ac
mochichi <moyurusuto@gmail.com>
2019-09-26 22:09:51 +0900

    a

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+rl.on('line', (lineString) => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[1];
+    const popu = parseInt(columns[3]);
+    if (year === 2010|| year === 2015) {
+        console.log(year);
+        console.log(prefecture);
+        console.log(popu);
+    }
+})
