b88303701b7400a7be10b5207529f9d7bec269a7
mochichi <moyurusuto@gmail.com>
2019-09-27 11:37:45 +0900

    a

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
-const prefectureDateMap = new Map(); //key:都道府県　value: 集計データのオブジェクト
+const prefectureDetaMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 rl.on('line', (lineString) => {
     const columns = lineString.split(',');
     const year = parseInt(columns[0]);
     const prefecture = columns[1];
     const popu = parseInt(columns[3]);
     if (year === 2010 || year === 2015) {
-        let value = prefectureDateMap.get(prefecture);
+        let value = prefectureDetaMap.get(prefecture);
         if (!value) {
             value = {
                 popu10: 0,
@@ -21,12 +21,19 @@ rl.on('line', (lineString) => {
         if (year === 2010) {
             value.popu10 = popu;
         }
-        if (yaer === 2015) {
+        if (year === 2015) {
             value.popu15 = popu;
         }
-        prefectureDataMap.set(prefecture, value);
+        prefectureDetaMap.set(prefecture, value);
     }
 });
 rl.on('close', () => {
-    console.log(prefectureDateMap);
-})
+    for (let [key,value] of prefectureDetaMap) {
+        value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+        return pair2[1].change - pair1[1].change;
+    });
+    console.log(rankingArray);
+    
+});
